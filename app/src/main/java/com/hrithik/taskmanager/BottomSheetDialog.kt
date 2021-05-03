d6d79f9a5af47d92eedba016a914dee530b26ba0
Hrithik Agarwal <agarwal.hrithik3961@gmail.com>
2021-05-03 23:46:21 +0530

    Initial commit

---
+++ app/src/main/java/com/hrithik/taskmanager/BottomSheetDialog.kt
@@ -0,0 +1,24 @@
+package com.hrithik.taskmanager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetDialog: BottomSheetDialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottom_sheet, container, false)
+    }
+
+}
